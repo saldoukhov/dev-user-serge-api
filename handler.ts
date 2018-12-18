ba6bcf40887827945852410786aa7d61d493067b
Sergey Aldoukhov <saldoukhov@gmail.com>
2018-12-17 18:04:05 -0800

    init

---
+++ handler.ts
@@ -0,0 +1,20 @@
+import {ApolloServer, gql} from "apollo-server-lambda";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!',
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+export const handleRequest = server.createHandler();
